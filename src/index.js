c985f01b47eae7cf07e586de46b8250b6a711e87
beeebeep <YouCanWriteMe@ya.ru>
2021-03-07 03:30:17 +0700

    fix: implement functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (number === 0) {
+        return "zero";
+    }
+
+    let littleNums = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    let decimalNames = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let depth = ["", "hundred", "thousand"];
+
+    let hrn = "";
+
+    let currentDepth = 0;
+
+    while (true) {
+        if (number % 100 !== 0) {
+            let t = "";
+            if (number % 100 < 20) {
+                t = littleNums[number % 100];
+            } else {
+                t =
+                    decimalNames[((number / 10) ^ 0) % 10] +
+                    (littleNums[number % 10] === "" ? "" : " ") +
+                    littleNums[number % 10];
+            }
+            hrn =
+                t +
+                (depth[currentDepth] === "" ? "" : " ") +
+                depth[currentDepth] +
+                (hrn === "" ? "" : " ") +
+                hrn;
+        }
+        number = (number / 100) ^ 0;
+        if (number === 0) {
+            break;
+        }
+        currentDepth++;
+    }
+
+    return hrn;
+};
